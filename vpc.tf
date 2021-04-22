210cff4d5f2d308f02da354f60f5fa63849c3e19
palvez <69176838+palvez@users.noreply.github.com>
2021-04-21 22:27:10 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -23,6 +23,7 @@ resource "aws_subnet" "vpc-subnet-us-east-1a" {
   vpc_id     = aws_vpc.test-terraform-vpc.id
   cidr_block = "172.16.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "VPC US-East-1a"
